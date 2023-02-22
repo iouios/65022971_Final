08878f288b10fa1535511d79c0ded8edfa79eced
65022971_re-Score <119997906+iouios@users.noreply.github.com>
2023-02-22 15:01:56 +0700

    S

---
+++ js1.js
@@ -0,0 +1,11 @@
+function BlurBox(B) {
+    T.style.background = "white";
+}
+function myFunction() {
+    alert("ข้อมูลไม่ครบ");
+
+function S {
+    let NameInput = document.getElementById("NameInput");
+    let Name = NameInput.value;
+    console.log(Name);
+}
